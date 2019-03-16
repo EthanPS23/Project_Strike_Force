fdf57311fc7b2ad63ad5fbb7f6f84d92ec9643ad
bgezekiel <44778576+bgezekiel@users.noreply.github.com>
2019-03-16 13:18:34 -0600

    Added DBConnect Class

---
+++ Java Desktop Application/src/sample/DBConnect.java
@@ -0,0 +1,19 @@
+package sample;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnect {
+    public static Connection getConnection()
+    {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/travelexperts", "harv", "password");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+}
