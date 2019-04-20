927ae096cca355a60be603ede22648cc91898fab
EthanPS23 <ethanps23@gmail.com>
2019-04-20 12:07:05 -0600

    creation of login page

--- TravelExpertsWebApp/src/RegistrationServlet.java
+++ TravelExpertsWebApp/src/RegistrationServlet.java
@@ -2,12 +2,14 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.*;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class RegistrationServlet
@@ -16,6 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 public class RegistrationServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	PrintWriter out;
+	HttpSession session;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -61,16 +64,16 @@ public class RegistrationServlet extends HttpServlet {
 						return "**Last name must have a value!**";
 					
 					case 2:
-						break;						
+						return "**Address must have a value!**";						
 					
 					case 3:
 						return "**You must include your city name!**";
 						
 					case 4:
-						return "**You must include your province or state name!**";
+						return "**You must include your country name!**";
 						
 					case 5:
-						return "**You must include your country name!**";
+						return "**You must include your province or state name!**";
 						
 					case 6:
 						return "**You must include your postal or zip code!**";
@@ -79,11 +82,13 @@ public class RegistrationServlet extends HttpServlet {
 						return "**Please provide your home phone number!**";
 						
 					case 8:
-						break;
+						return "**Please provide your business phone number!**";
 						
 					case 9:
-
-						break;
+						return "**Please provide your email!**";
+						
+					case 10:
+						return "**Please provide your password!**";
 					
 					default:
 					return "field must have a value.";
@@ -100,6 +105,7 @@ public class RegistrationServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		//doPost(request, response);
 		out = response.getWriter();
+		session = request.getSession();
 		String [] customerArray = new String[12];
 		customerArray[0] = request.getParameter("CustFirstName");
 		customerArray[1] = request.getParameter("CustLastName");
@@ -113,27 +119,70 @@ public class RegistrationServlet extends HttpServlet {
 		customerArray[9] = request.getParameter("CustEmail");
 		customerArray[10] = request.getParameter("CustPassword");
 		customerArray[11] = request.getParameter("AgentID");
-		out.print("<!doctype html>");
-		out.print("<html>");
-		out.print("<head><title>U.S Miles per gellon </title></head>");
-		out.print("version 2<body>");
-		out.print("" + customerArray[0] + "");
-		out.print("<h1>" + customerArray[1] + "</h1>");
-		out.print("<h1>" + customerArray[2] + "</h1>");
-		out.print("<h1>" + customerArray[3] + "</h1>");
-		out.print("<h1>" + customerArray[4] + "</h1>");
-		out.print("<h1>" + customerArray[5] + "</h1>");
-		out.print("<h1>" + customerArray[6] + "</h1>");
-		out.print("<h1>" + customerArray[7] + "</h1>");
-		out.print("<h1>" + customerArray[8] + "</h1>");
-		out.print("<h1>" + customerArray[9] + "</h1>");
-		out.print("<h1>" + customerArray[10] + "</h1>");
-		out.print("<h1>" + customerArray[11] + "</h1>");
-		out.print("<a href='converter.html'>Convert ANother on</a>");
-		out.print("</body>");
-		out.print("</html>");
 		
-		validate(customerArray);
+		String valid= validate(customerArray);
+		if (valid != "") {
+			session.setAttribute("message", valid);
+			response.sendRedirect("Registration.jsp");
+		}else {
+			//String msg = createCustomer(customerArray);
+			if (!createCustomer(customerArray)) {
+				session.setAttribute("message", "I dont know what happened");
+				response.sendRedirect("Registration.jsp");
+			}else {
+				response.sendRedirect("Login.jsp");
+			}
+		}
+	}
+	public boolean createCustomer(String [] customerArray)
+	{
+		String sql = "INSERT INTO customers ("
+			+ "CustomerID, CustFirstName, CustLastName, CustAddress, CustCity, CustProv, CustCountry, CustPostal, CustHomePhone, CustBusPhone, CustEmail, CustPassword, AgentID"
+			+ ") values (last_insert_id(), "
+			+ "'" + customerArray[0] + "'," 
+			+ " '" + customerArray[1] + "',"
+			+ " '" + customerArray[2] + "',"
+			+ " '" + customerArray[3] + "',"
+			+ " '" + customerArray[4] + "',"
+			+ " '" + customerArray[5] + "',"
+			+ " '" + customerArray[6] + "',"
+			+ " '" + customerArray[7] + "',"
+			+ " '" + customerArray[8] + "',"
+			+ " '" + customerArray[9] + "',"
+			+ " '" + customerArray[10] + "',"
+			+ " " + customerArray[11] + ")";
+	    try
+	    {
+	        //Class.forName("com.mysql.jdbc.Driver");
+			//Class.forName("oracle.jdbc.driver.OracleDriver");
+			Class.forName("org.mariadb.jdbc.Driver");
+	        //Connection dbConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/travelexperts","root","password");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/travelexperts","harv","password");
+	        //Connection dbConn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orant11g", "ictoosd", "ictoosd");
+
+	        Statement stmt = conn.createStatement();
+
+	        int rows = stmt.executeUpdate(sql);
+
+	        // Cleanup
+	        conn.close();  // close the connection
+		        
+			if (rows == 1)
+			{
+				//return "Your registration was a success. Thank you for joining The Travel Experts.";
+				return true;
+			}
+			else
+			{
+				//return "You were unsuccessful";
+				return false;
+			}
+	    }
+	    catch (Exception excp)
+	    {
+	        excp.printStackTrace();
+	    }
+		return false;
 	}
 
 }
